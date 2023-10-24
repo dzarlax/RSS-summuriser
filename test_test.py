31c38ca5dab1f37847f61ba3941d642296cfb622
Alexey Panfilov <alexey@panfilov.net>
2023-10-24 15:51:59 +0200

    New tests

--- test_test.py
+++ test_test.py
@@ -1,47 +1,92 @@
 import pytest
-from unittest.mock import patch, MagicMock
-from main import get_iam_api_token, count_tokens, process_entry
-from datetime import datetime
+from main import (load_config, get_iam_api_token, count_tokens,
+                         get_previous_feed_and_links, upload_file_to_yandex,
+                         extract_image_url, process_entry)
+
+# For the purpose of demonstration, I'll assume the name of your module is 'your_module'
+
+
+def test_load_config():
+    # Assuming you have a key "service_account_id" in your config.json
+    result = load_config("service_account_id")
+    assert isinstance(result, str)  # Assuming service_account_id is a string
+
 
 def test_get_iam_api_token():
-    # Имитируем успешный ответ от IAM
-    with patch('requests.post') as mocked_post:
-        mocked_response = MagicMock()
-        mocked_response.json.return_value = {'iamToken': 'sample_token'}
-        mocked_response.raise_for_status.return_value = None
-        mocked_post.return_value = mocked_response
+    token = get_iam_api_token()
+    assert isinstance(token, str)
 
-        result = get_iam_api_token('sa_id', 'key_id', 'https://iam_url', 'private_key')
-        assert result == 'sample_token'
 
-def test_count_tokens():
-    # Имитируем успешный ответ от API токенизации
-    with patch('requests.post') as mocked_post:
-        mocked_response = MagicMock()
-        mocked_response.json.return_value = {'tokens': [1, 2, 3]}
-        mocked_post.return_value = mocked_response
+def test_count_tokens(mocker):
+    # Mocking the request.post response
+    mocker.patch('requests.post', return_value=MockResponse({"tokens": ["token1", "token2"]}, 200))
+    result = count_tokens("sample text", "dummy_api_key", "dummy_folder_id")
+    assert result == 2
 
-        result = count_tokens('sample_text', 'api_key')
-        assert result == 3
 
-def test_process_entry():
-    # Тест для проверки функции process_entry
-    entry = MagicMock()
-    entry.link = 'https://example.com'
-    entry.published = 'Wed, 20 Oct 2023 12:00:00 +0000'
-    entry.title = 'Sample title'
-    entry.summary = 'Sample summary'
-    
-    with patch('trafilatura.fetch_url') as mocked_fetch, \
-     patch('trafilatura.extract') as mocked_extract, \
-     patch('main.summarize') as mocked_summarize:
-        mocked_fetch.return_value = None
-        mocked_extract.return_value = 'Extracted text'
-        mocked_summarize.return_value = 'Summarized text'
-        
-        two_days_ago = datetime.strptime('Mon, 18 Oct 2023 12:00:00 +0000', '%a, %d %b %Y %H:%M:%S %z').replace(tzinfo=None)
-    result = process_entry(entry, two_days_ago, 'api_key', [])
-    
-    assert result is not None
-    assert result['description'] == 'Summarized text'
-    assert result['title'] == 'Sample title'
+def test_get_previous_feed_and_links(mocker):
+    # Mocking the s3.get_object response
+    mocker.patch('boto3.client.get_object', return_value={"Body": MockBody("<rss></rss>")})
+    parsed_rss, links = get_previous_feed_and_links("dummy_bucket", "dummy_s3_client", "dummy_object_name")
+    assert isinstance(parsed_rss, feedparser.FeedParserDict)
+    assert isinstance(links, list)
+
+
+def test_upload_file_to_yandex(mocker):
+    # Mocking the s3.upload_file method
+    mocker.patch('boto3.client.upload_file', return_value=None)
+    # This test just checks if the function runs without exceptions, as it doesn't return anything
+    upload_file_to_yandex("dummy_file", "dummy_bucket", "dummy_s3_client", "dummy_object_name")
+
+
+def test_extract_image_url():
+    html_content = """
+    <html>
+        <head>
+            <meta property="og:image" content="https://example.com/image.jpg">
+        </head>
+    </html>
+    """
+    result = extract_image_url(html_content, "default_logo_url")
+    assert result == "https://example.com/image.jpg"
+
+    # Test with no og:image meta tag
+    html_content_no_image = "<html></html>"
+    result_no_image = extract_image_url(html_content_no_image, "default_logo_url")
+    assert result_no_image == "default_logo_url"
+
+
+@pytest.mark.parametrize(
+    "entry, two_days_ago, api_key, previous_links, logo, API_URL, folder_id, expected",
+    [
+        # Add your test cases here
+    ]
+)
+def test_process_entry(entry, two_days_ago, api_key, previous_links, logo, API_URL, folder_id, expected):
+    result = process_entry(entry, two_days_ago, api_key, previous_links, logo, API_URL, folder_id)
+    assert result == expected
+
+
+class MockResponse:
+    def __init__(self, json_data, status_code):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_data
+
+    def raise_for_status(self):
+        if 400 <= self.status_code < 600:
+            raise requests.RequestException()
+
+
+class MockBody:
+    def __init__(self, content):
+        self.content = content
+
+    def read(self):
+        return self.content
+
+
+if __name__ == "__main__":
+    pytest.main()
