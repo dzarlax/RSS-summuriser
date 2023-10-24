8df79711c604052e8a58bd7e544934cb60a4c83b
Alexey Panfilov <alexey@panfilov.net>
2023-10-24 22:33:07 +0200

    fix json

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from typing import Optional
