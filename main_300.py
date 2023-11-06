ffa37552144e107167297e6383026501a12027b6
Alexey Panfilov <alexey@panfilov.net>
2023-11-06 16:59:27 +0100

    add logging

--- main_300.py
+++ main_300.py
@@ -109,6 +109,7 @@ def ya300(link, endpoint, token):
             headers={'Authorization': F"OAuth {token}"}
         )
         response_data = response.json()
+        print(response)
         print(response_data)
         print(response.status_code)
         url = response_data.get("sharing_url", None)
